993df3a32e3ac1a12e3bf8e4cc717cb59211045c
Cyma-s <ssol0319@naver.com>
2022-04-29 11:25:44 +0900

    feat(MainActivity.kt): splash activity created

--- app/src/main/java/com/example/asteroid/MainActivity.kt
+++ app/src/main/java/com/example/asteroid/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.asteroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.asteroid.login.StartPage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val nextIntent = Intent(this, StartPage::class.java)
+        startActivity(nextIntent)
     }
 }
---
+++ app/src/main/java/com/example/asteroid/login/StartPage.kt
@@ -0,0 +1,12 @@
+package com.example.asteroid.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.asteroid.R
+
+class StartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+    }
+}
